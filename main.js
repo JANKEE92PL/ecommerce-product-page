9c4c65f1715ce7f0f048345cbac791992014d6d9
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-04-14 15:05:24 +0200

    finish logic in JS

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const get = (element) => {
 let amount = get("amount");
 let minus = get("minus");
 let plus = get("plus");
+let addBtn = get("addBtn");
+let cart = [];
+let item = "";
 
 plus.addEventListener("click", (e) => {
   amount.stepUp(1);
@@ -11,3 +14,23 @@ plus.addEventListener("click", (e) => {
 minus.addEventListener("click", (e) => {
   amount.stepDown(1);
 });
+
+addBtn.addEventListener("click", (e) => {
+  amountInt = getAmount(amount);
+  addToCart(getItem());
+  console.table(cart);
+});
+
+const addToCart = (item) => {
+  cart.push(item);
+};
+
+const getItem = () => {
+  item = document.getElementsByClassName("card-title")[0].firstChild.nodeValue;
+  return item;
+};
+
+const getAmount = (target) => {
+  amountInt = target.value;
+  return amountInt;
+};
